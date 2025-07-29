81547b601f35064e24d186f7e208fb9f7bc69c3f
sonali270702 <104364617+sonali270702@users.noreply.github.com>
2025-07-29 16:58:09 +0530

    Add Number guessing game

--- sona-task/python_basic.py
+++ sona-task/python_basic.py
@@ -8,8 +8,9 @@ print(sum)
 
 
 
-
-
+###############################################
+###### choice random number from list ###########
+###############################################
 
 import random 
 
@@ -17,3 +18,29 @@ l1=[1,2,"sonali",3.23]
 b=random.choice(l1)
 print(b)
 
+############################### Number guessing game ######################
+print("Welcome to the number guessing game !")
+
+print("thinking the number between 1 to 100 ")
+
+attempts=10
+
+print(f"you have {attempts} attempts to guess the number ")
+
+random_number=random.randint(1,101)
+
+
+
+while attempts>0:
+    print(f"you have {attempts} attempts ")
+    guess=int(input("make a guess: "))
+    
+    if guess==random_number:
+        print(f"you're correct the number is : {random_number}")
+        break
+    elif guess >random_number:
+        print(f" Too high try Again !!")
+    else:
+        print("Too low try Again !!") 
+        
+    attempts -=1
