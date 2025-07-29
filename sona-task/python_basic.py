a78672849ecf01890495d6266622ddb6610d5b7e
sonali270702 <104364617+sonali270702@users.noreply.github.com>
2025-07-29 16:24:00 +0530

    Add new task in file to choice random number from given list

---
+++ sona-task/python_basic.py
@@ -0,0 +1,19 @@
+######################################
+########## Adding two numbers ########
+######################################
+num1=10
+num2=20
+sum=num1+num2
+print(sum)
+
+
+
+
+
+
+import random 
+
+l1=[1,2,"sonali",3.23]
+b=random.choice(l1)
+print(b)
+
