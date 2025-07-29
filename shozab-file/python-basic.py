ab100873fffdec00c2dc0d554f4dfe5c45da3ddd
Shozab2001 <shozabkaif@gmail.com>
2025-07-29 17:45:40 +0530

    Added If Else Conditions

--- shozab-file/python-basic.py
+++ shozab-file/python-basic.py
@@ -7,3 +7,15 @@ num1 = 5
 num2 = 3
 sum = num1 + num2
 print("The sum is:", sum)
+
+
+
+# Check if a number is positive or negative
+number = -7
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
