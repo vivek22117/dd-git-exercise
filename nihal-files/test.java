3b7a4a98ef55df4f170e4de9fab9551e217e97f0
nihalmore05 <nihalmore05@gmail.com>
2025-07-29 18:04:39 +0530

    Adding some changes

--- nihal-files/test.java
+++ nihal-files/test.java
@@ -2,7 +2,15 @@
 public class test{
 
     public static void main(String[] args) {
+
+        int a = 10;
+        int b = 25;
+
+        int sum = a + b;
+        
+        System.out.println("a + b = " + sum);
         
         System.out.println("hello java");
     }
 }
+
