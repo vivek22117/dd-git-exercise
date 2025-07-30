4f844af784b59389490ce96e8b4855bab3986a46
Harshalpatil028 <hp2509413@gmailcom>
2025-07-30 12:00:52 +0530

    addded object code

--- harshal-NewButton/Fact.java
+++ harshal-NewButton/Fact.java
@@ -1,4 +1,19 @@
 import java.util.Scanner;
+
+class car {
+    class Car {
+    String brand;
+    int year;
+
+    Car(String b, int y) {
+        brand = b;
+        year = y;
+    }
+
+    void displayInfo() {
+        System.out.println("Brand: " + brand + ", Year: " + year);
+    }
+}
 public class Fact {
     public static void main(String[] args) {
         System.out.println("things went to above ");
