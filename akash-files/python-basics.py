cb1d17a25f3781b2dab3fc13a00699462c6865d0
aka156 <akashspimpale@gmail.com>
2025-07-30 15:41:19 +0530

    Add new basic code of float function

--- akash-files/python-basics.py
+++ akash-files/python-basics.py
@@ -20,3 +20,12 @@ while count <= 5:
 print("Name:", student["name"])
 print("Age:", student["age"])
 print("Grade:", student["grade"])
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b)
