0f7ece7e2b466e4492bbb38f54e0c8db2fbeeade
Harshalpatil028 <hp2509413@gmailcom>
2025-07-30 16:06:04 +0530

    second commit of ssh exercise

--- harshal-file/ssh-exercise.sh
+++ harshal-file/ssh-exercise.sh
@@ -7,3 +7,14 @@
 echo "Updating and upgrading system packages..."
 sudo apt-get update && sudo apt-get upgrade -y
 echo "Package update complete."
+
+
+# --- Step3: Add new user ---
+# Replace 'newuser' with the actual username.
+sudo adduser newuser
+
+# --- Step 2: Add SSH Key for a New User ---
+# Replace 'newuser' with the actual username.
+# Replace 'ssh-rsa AAAA...' with the user's actual public SSH key.
+
+echo "Configuring SSH access for user: $USERNAME"
