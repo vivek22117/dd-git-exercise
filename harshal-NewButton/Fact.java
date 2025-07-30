cff7c0e8bc282e1dad2bddfbeaca26ab730a2cfb
Harshalpatil028 <hp2509413@gmailcom>
2025-07-30 11:58:44 +0530

    added sum code

--- harshal-NewButton/Fact.java
+++ harshal-NewButton/Fact.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Fact {
     public static void main(String[] args) {
         System.out.println("things went to above ");
+         System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
 }
