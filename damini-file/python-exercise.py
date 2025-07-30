e90914e83ae4666dbe795d434dc7e446f11abbd0
developer <daminipatil2910@gmail.com>
2025-07-30 12:22:02 +0530

    first commit

---
+++ damini-file/python-exercise.py
@@ -0,0 +1,14 @@
+num = int(input("give any number : "))
+print(num)
+if num % 2 == 0:
+  print("number is even")
+else:
+  print("number is odd")
+even_list =[]
+odd_list =[]
+for i in range(1,11):
+  if i % 2 != 0:
+    print(i)
+    odd_list.append(i)
+  else:
+    even_list.append(i) 
