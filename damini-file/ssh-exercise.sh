5c02a88f722cc555dd5c80a8be3ce5ec2603e146
developer <daminipatil2910@gmail.com>
2025-07-30 15:57:43 +0530

    change in notes

--- damini-file/ssh-exercise.sh
+++ damini-file/ssh-exercise.sh
@@ -8,11 +8,11 @@ echo "Updating and upgrading system packages..."
 sudo apt-get update && sudo apt-get upgrade -y
 echo "Package update complete."
 
-# --- Step3: Add new user ---
+# --- Step2: Add new user ---
 # Replace 'newuser' with the actual username.
 sudo adduser newuser
 
-# --- Step 2: Add SSH Key for a New User ---
+# --- Step 3: Add SSH Key for a New User ---
 # Replace 'newuser' with the actual username.
 # Replace 'ssh-rsa AAAA...' with the user's actual public SSH key.
 
