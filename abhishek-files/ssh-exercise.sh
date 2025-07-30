6c965bde33bd4748c6fad3977f0fd194fe84e5b8
abhishekKadale1809 <abhishekkadale779@gmail.com>
2025-07-30 15:58:40 +0530

    step-2 Add new user

--- abhishek-files/ssh-exercise.sh
+++ abhishek-files/ssh-exercise.sh
@@ -8,6 +8,12 @@ echo "Updating and upgrading system packages..."
 sudo apt-get update && sudo apt-get upgrade -y
 echo "Package update complete."
 
+
+# --- Step3: Add new user ---
+# Replace 'newuser' with the actual username.
+sudo adduser newuser
+
+
 # --- Step 2: Add SSH Key for a New User ---
 # Replace 'newuser' with the actual username.
 # Replace 'ssh-rsa AAAA...' with the user's actual public SSH key.
