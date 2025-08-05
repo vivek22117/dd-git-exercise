0c7c52b5298814963f867d1a20740b4c4e20be69
Harshalpatil028 <hp2509413@gmailcom>
2025-08-05 16:27:13 +0530

    convert .java file into .class file by Harshal

--- harshal-file/index.java
+++ harshal-file/index.java
@@ -1,7 +1,7 @@
-public class index
-{
+public class index {
     public static void main(String[] args) {
-        String nae="harshal";
-        System.out.println("name is "+nae);
+        String name = "harshal";
+       
+        System.out.println("here name is " + name);
     }
 }
