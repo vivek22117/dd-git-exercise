5e32683ff6f5e0ba6abc1acafe595a33329e3ebe
Harshalpatil028 <hp2509413@gmailcom>
2025-08-12 14:15:57 +0530

    reverse the string using charAt method storing in an empty array

---
+++ harshal-file/StringReverser.java
@@ -0,0 +1,25 @@
+
+public class StringReverser {
+
+    public static void main(String[] args) {
+        // --- Test Cases ---
+        String original1 = "engineer";
+        String result1 = bruteForceReverse(original1);
+        System.out.println("Reversed: " + result1); // Expected: reenigne
+
+        String original2 = "Java";
+        String result2 = bruteForceReverse(original2);
+        System.out.println("Reversed: " + result2); // Expected: avaJ
+    }
+
+    // Your method here
+    public static String bruteForceReverse(String text) {
+        String emptyArray = "";
+     
+        for (int i = 0; i < text.length(); i++) {
+            char   ch = text.charAt(i);//J a v a
+            emptyArray = ch + emptyArray;
+        }
+        return emptyArray; // Placeholder
+    }
+}
