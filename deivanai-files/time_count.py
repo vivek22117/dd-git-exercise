5f4cbd05b06c39f1ce2995065134ca17254d33ba
Deiva-12 <deivanaii0405@gmail.com>
2025-08-13 12:59:05 +0530

    function python exercise3 by Deivanai

---
+++ deivanai-files/time_count.py
@@ -0,0 +1,13 @@
+
+def countdown(cound):
+    print("Starting {} second countdown:".format(cound))
+    while cound >0:
+        print(cound)
+        cound-=1
+    print("Blast off") 
+
+countdown(8)   
+        
+
+   
+
