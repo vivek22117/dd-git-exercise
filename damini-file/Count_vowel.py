8e5d94a3c8a17cd7f90fd1b10b25ce0d3c733839
developer <daminipatil2910@gmail.com>
2025-08-13 13:44:39 +0530

    countdown and count vowel functions are done

---
+++ damini-file/Count_vowel.py
@@ -0,0 +1,21 @@
+#code here
+def count_vowels(input_string):
+  vowels="aeiou"
+  count=0
+  input_string = input_string.lower()
+  print(input_string)
+  for i in input_string:
+    if i in vowels:
+      count+=1
+  print(count)
+
+count_vowels("DJHFKSJDNVMDNLSJDPOMDKLFOERIWKENWEQA")
+
+      
+      
+
+  
+
+
+  
+
