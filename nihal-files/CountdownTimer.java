ca02817337ed7b660737fca8dad7f464135bb252
nihalmore05 <nihalmore05@gmail.com>
2025-08-13 15:42:08 +0530

    13 Aug 25 Exercise Done CountDowTime and GradeCalculator

---
+++ nihal-files/CountdownTimer.java
@@ -0,0 +1,23 @@
+public class CountdownTimer {
+
+    public static void main(String[] args) {
+        // --- Test Case ---
+        System.out.println("Starting 5-second countdown:");
+        countdown(5);        
+        // Expected Output:
+        // 5
+        // 4
+        // 3
+        // 2
+        // 1
+        // Blast off!
+    }
+
+    // Your method here
+    public static void countdown(int startNum) {
+        while(startNum >=1){                              
+           System.out.println(startNum);
+           startNum--; // startnum =startnumber-1
+        }
+    }
+}
