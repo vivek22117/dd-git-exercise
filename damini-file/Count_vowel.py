d839a1a73b04ad97c2a7c2bad42b1735314bac0f
developer <daminipatil2910@gmail.com>
2025-08-13 14:09:34 +0530

    chnges in exercise

--- damini-file/Count_vowel.py
+++ damini-file/Count_vowel.py
@@ -3,13 +3,15 @@ def count_vowels(input_string):
   vowels="aeiou"
   count=0
   input_string = input_string.lower()
-  print(input_string)
+  #print(input_string)
   for i in input_string:
     if i in vowels:
       count+=1
-  print(count)
+  return count
 
-count_vowels("DJHFKSJDNVMDNLSJDPOMDKLFOERIWKENWEQA")
+# count_vowels("DJHFKSJDNVMDNLSJDPOMDKLFOERIWKENWEQA")
+input_string= "Hello World"
+print(f"Vowel count in '{input_string}':  {count_vowels(input_string)}")
 
       
       
--- damini-file/Countdown_function.py
+++ damini-file/Countdown_function.py
@@ -3,11 +3,10 @@ def Countdown(positive_count):
   if positive_count < 0:
     print("please provide positive integer only")
   else:
-    print(f"Starting {positive_count}-second")
+    print(f"Starting {positive_count}-second countdown:")
     while positive_count>0:
       print(positive_count)
       positive_count-=1
     print("Blast off!")
-
 Countdown(10)
 
