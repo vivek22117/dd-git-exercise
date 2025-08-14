87937e6bb7150e3efbb87d2be200181a1e49a07e
Harshalpatil028 <hp2509413@gmailcom>
2025-08-14 13:00:43 +0530

    both code Filter even number and find user roles

--- harshal-file/CountdownTimer.java
+++ harshal-file/CountdownTimer.java
@@ -4,20 +4,12 @@ public class CountdownTimer {
         // --- Test Case ---
         System.out.println("Starting 5-second countdown:");
         countdown(5);
-        // Expected Output:
-        // 5
-        // 4
-        // 3
-        // 2
-        // 1
-        // Blast off!
     }
 
     // Your method here
     public static void countdown(int startNum) {
-       for(int i=startNum;i>=0;i--)
-       {
-        System.out.println(i);
-       }
+        for (int i = startNum; i >= 0; i--) {
+            System.out.println(i);
+        }
     }
 }
--- harshal-file/StringReverser.java
+++ harshal-file/StringReverser.java
@@ -23,3 +23,5 @@ public class StringReverser {
         return emptyArray; // Placeholder
     }
 }
+
+
