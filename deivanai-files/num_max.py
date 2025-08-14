fc0f1920946d44f974674818d7e525a7954e0002
Deiva-12 <deivanaii0405@gmail.com>
2025-08-14 12:51:06 +0530

    function python exercise6 by Deivanai

--- deivanai-files/num_max.py
+++ deivanai-files/num_max.py
@@ -1,10 +1,11 @@
 def  find_max(number):
+    
     max_num = number[0]
     for valu in number:
-        if valu > max_num:
+        if valu < max_num:
             max_num = valu
     return max_num
-numbers1 = [1,41, 12, 9, 74, 15,h]
+numbers1 = [1,41, 12, 9, 74, 15]
 print(f"The max number in {numbers1} is: {find_max(numbers1)}") 
 
 numbers2 = [-10, -5, -2, -1]
