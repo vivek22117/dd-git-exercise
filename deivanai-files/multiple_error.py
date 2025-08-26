663a7fff3f3a0cb2637960c279029e32111213c0
Deiva-12 <deivanaii0405@gmail.com>
2025-08-26 10:11:07 +0530

    Python Exception exercise3 by Deivanai

--- deivanai-files/multiple_error.py
+++ deivanai-files/multiple_error.py
@@ -14,3 +14,15 @@ my_list = [0,10, "hello", 40,"a+2"]
 process_item(my_list, 1)  # Success
 process_item(my_list, 0) # Triggers IndexError
 process_item(my_list, 5)  # Triggers TypeError
+
+#________________________________________________________________________________________________________
+
+try:
+    with open("non_existent_file.txt", "r") as file:
+        content = file.read()
+except FileNotFoundError as e:
+    print("Failed to open file.")
+    print(f"Reason: {e}") # 'e' is the exception object, which contains the message
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
