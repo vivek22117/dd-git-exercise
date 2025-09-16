071064a8e8811dc019e946749dff6843d37c32a7
nihalmore05 <nihalmore05@gmail.com>
2025-09-16 15:20:46 +0530

    Array  duplicate task

---
+++ nihal-files/Array_Duplicat.java
@@ -0,0 +1,12 @@
+
+public class Array_Duplicat {
+
+
+    public static void fun1(){
+        System.out.println("Enter youar name: "+ "nihal" );
+    }
+    public static void main(String[] args) {
+        // int array[]={1,2,3,1,4,2,5,6,7,8,4};
+          fun1();
+    }
+}
